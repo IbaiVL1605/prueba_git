b65d9794ae1d99f9d9a97addd2730bb8feb51a50
Ibai Velada Lopez <ibai.velada@opendeusto.es>
2024-09-23 08:57:20 +0200

    test

--- git/src/git/main.java
+++ git/src/git/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("estamos a lunes");
+		System.out.println("test1");
 	}
 
 }
